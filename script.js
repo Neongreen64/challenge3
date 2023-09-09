aa98021e36a5b2f7f22e5ff0b3e3dc04d4327f04
Grant <dogmanyo@gmail.com>
2023-09-09 17:03:03 -0400

    added comments

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
+  //Prepares variables to hold the different types of password criteria.
   var password = "";
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
   var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -9,16 +10,31 @@ function generatePassword(){
   var number = "0123456789";
   var generator = "";
 
+  //While the length of pass is less than 8 or greater than 128 it will continue to prompt the user.
   var lengthPass = prompt("Length of password (8-128 characters): ");
-  if (lengthPass > 128 || lengthPass < 8 ){
+  while (lengthPass > 128 || lengthPass < 8 ){
     lengthPass = prompt("Length of password (8-128 characters): ");
   }
 
+  //While the answers to the prompts are not equal to yes or no it will continue to prompt the user.
   var lowercasePass = prompt("Include lowercase letters?: ");
+  while (lowercasePass !== "yes" && lowercasePass !== "no"){
+    lowercasePass = prompt("Include lowercase letters?: ");
+  }
   var uppercasePass = prompt("Include uppercase letters?: ");
+  while (uppercasePass !== "yes" && uppercasePass !== "no"){
+    uppercasePass = prompt("Include uppercase letters?: ");
+  }
   var specialPass = prompt("Include special characters?: ");
+  while (specialPass !== "yes" && specialPass !== "no"){
+    specialPass = prompt("Include special characters?: ");
+  }
   var numberPass = prompt("Include numbers?: ");
+  while (numberPass !== "yes" && numberPass !== "no"){
+    numberPass = prompt("Include numbers?: ");
+  }
   
+  //If the user selects yes on one of the prompts the generator gets the corresponding string concatenated.
   if (lowercasePass === "yes"){
     generator += lowercase;
   }
@@ -32,6 +48,7 @@ function generatePassword(){
     generator += number;
   }
 
+  //Generates a password by grabbing a random character from the generator string and adds it to the password string.
   for (var x = 0; x < lengthPass; x++){
     var character = Math.floor(Math.random() * generator.length + 1);
     password += generator.charAt(character);
